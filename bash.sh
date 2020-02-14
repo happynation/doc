1fb134d6d818c7d44ddd4cad5bf53b06d51d6a01
Volodymyr Kulyk <44554196+rootvovak@users.noreply.github.com>
2020-02-14 17:39:30 -0600

    Delete bash.sh

--- bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo yum install wget unzip -y
-sudo wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
-sudo unzip terraform_0.11.14_linux_amd64.zip
-sudo mv terraform /bin
