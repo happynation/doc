4fb910eae4288e1995ed3a666dc61aaa41bf87fe
Volodymyr Kulyk <44554196+rootvovak@users.noreply.github.com>
2020-02-14 17:15:35 -0600

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
-unzip terraform_0.11.14_linux_amd64.zip
-mv terraform /bin
+sudo yum install wget unzip -y
+sudo wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+sudo unzip terraform_0.11.14_linux_amd64.zip
+sudo mv terraform /bin
